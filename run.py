3d311e015ef80371716664d46afcabf6d0e6f6c1
zhg061 zhg061 <zhg061@dsmlp-login.ucsd.edu>
2020-11-24 16:22:39 -0800

    update run.py

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
 import sys
 import os
 import json
-
 sys.path.insert(0, 'src')
 
 def main(targets):
-    '''
-    Runs the main project pipeline logic, given the targets.
-    targets must contain: 'data'. 
-    
-    '''
+    if 'test' in targets:        
+        #generate test_1 & test_2
+        os.system('zcat /datasets/srp073813/SRR3438851_1.fastq.gz | head -100 > ~/dsc180a_genetics/test/test_data/m7_1.fastq')
+        os.system('zcat /datasets/srp073813/SRR3438851_2.fastq.gz | head -100 > ~/dsc180a_genetics/test/test_data/m7_2.fastq')
 
-    if 'data' in targets:
-        with open('config/data-params.json') as fh:
-            data_cfg = json.load(fh)
+        #FastQC report for test1 & test2
+        os.system('gzip ~/dsc180a_genetics/test/test_data/m7_1.fastq ~/dsc180a_genetics/test/test_data/m7_2.fastq')
+        os.system('/opt/FastQC/fastqc ~/dsc180a_genetics/test/test_data/m7_1.fastq.gz')
+        os.system('/opt/FastQC/fastqc ~/dsc180a_genetics/test/test_data/m7_2.fastq.gz')
 
-        # print the name of all the file in dataset into output.txt
-        print(os.listdir(data_cfg['path']), file=open("output.txt", "a"))
+        ## Kallisto        
+        # Quantification for test1 & test2
+        os.system('cd /opt/kallisto_linux-v0.42.4/test')
+        os.system('/opt/kallisto_linux-v0.42.4/kallisto quant -i /datasets/srp073813/reference/kallisto_transcripts.idx -o ~/dsc180a_genetics/test/test_kallisto -b 100 ~/dsc180a_genetics/test/test_data/m7_1.fastq.gz ~/dsc180a_genetics/test/test_data/m7_2.fastq.gz')
     return 
 
 if __name__ == '__main__':
